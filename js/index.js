a7a196021c45d515799abd94cebd5233f6c61e04
KrystianeOtis <oti19001@byui.edu>
2021-04-27 14:00:57 -0600

    js update

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ window.addEventListener('load', (event) => {
     const lu = document.querySelector('#last-updated');
     lu.textContent = document.lastModified;
 
-    const cry = document.querySelector('#copyrightyear');
+    const cry = document.querySelector('#copyright-year');
     cry.textContent = new Date().getFullYear();
 })
