a9b7af4ecf2b84cd66b7fcd7d494f30ef041deb0
KrystianeOtis <oti19001@byui.edu>
2021-05-07 16:50:22 -0600

    color scheme commit

--- lesson3/js/index.js
+++ lesson3/js/index.js
@@ -6,3 +6,11 @@ window.addEventListener('load', () => {
         mainnav.classList.toggle('responsive')
     }, false);
 });
+
+window.addEventListener('load', (event) => {
+    const lu = document.querySelector('#last-updated');
+    lu.textContent = document.lastModified;
+
+    const cry = document.querySelector('#copyright-year');
+    cry.textContent = new Date().getFullYear();
+});
