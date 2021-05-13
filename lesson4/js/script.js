6f605290d0d9bb6d0a1208eb49be96372de56a31
KrystianeOtis <oti19001@byui.edu>
2021-05-13 16:50:44 -0600

    javascript commit

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -7,10 +7,31 @@ window.addEventListener('load', () => {
     }, false);
 });
 
-window.addEventListener('load', (event) => {
-    const lu = document.querySelector('#last-updated');
-    lu.textContent = document.lastModified;
+/* Date format example : Wednesday, 24 July 2020.*/
 
-    const cry = document.querySelector('#copyright-year');
-    cry.textContent = new Date("January 25 2015").getFullYear();
-});
+window.addEventListener('load', (event) => {
+    const week = document.querySelector('#name-of-day');
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    week.textContent = weekday[d.getDay()];
+
+    const dayNum = document.getElementById('day');
+    dayNum.textContent = d.getDate();
+
+    var monthName = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const monthNum = document.getElementById('month');
+    monthNum.textContent = monthName[d.getMonth()];
+
+    const year = document.getElementById('year');
+    year.textContent = d.getFullYear();
+
+    const cry = document.querySelector("#copyright-year");
+    cry.textContent = d.getFullYear();
+})
