52dc2543196ed4df975ee017f6ee3bfd7f43a6ec
KrystianeOtis <oti19001@byui.edu>
2021-05-13 19:02:50 -0600

    fix date

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -10,7 +10,17 @@ window.addEventListener('load', () => {
 /* Date format example : Wednesday, 24 July 2020.*/
 
 window.addEventListener('load', (event) => {
-
+    const week = document.querySelector('#name-of-day');
+    var d = new Date();
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+    week.textContent = weekday[d.getDay()];
 
     const dayNum = document.getElementById('day');
     dayNum.textContent = d.getDate();
