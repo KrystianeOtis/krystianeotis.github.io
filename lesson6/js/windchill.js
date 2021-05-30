05977c0520382a8e33c2b2ca50e7efcad8397e43
KrystianeOtis <oti19001@byui.edu>
2021-05-29 22:22:15 -0600

    commit

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,21 @@
+/* Chilly weather*/
+
+window.addEventListener('load', () => {
+    const calcWindChill = (temperature, speed) => {
+        if (temperature <= 50 && speed > 3) {
+            return Math.round(
+                35.74 + (0.6215 * temperature) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * (temperature *
+                    Math.pow(speed, 0.16)))) + "°F"
+        } else {
+            return "NA"
+        }
+    }
+
+    const displayWindChill = () => {
+        let temperature = document.getElementById("temp").textContent;
+        let wind = document.getElementById("windSpeed").textContent;
+        let result = calcWindChill(temperature, wind);
+        document.getElementById("windChill").innerHTML = result;
+    }
+    displayWindChill();
+})
