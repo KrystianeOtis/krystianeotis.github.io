2e42979a157472248120555491fb1fa6f5438990
KrystianeOtis <oti19001@byui.edu>
2021-06-25 17:21:34 -0600

    windchill javascript

--- lesson10/js/prestonweather.js
+++ lesson10/js/prestonweather.js
@@ -38,3 +38,23 @@ async function getWeather() { // async function - The async and await keywords e
 window.addEventListener('load', (event) => { //registers when page is loaded
     getProphets();
 })
+
+window.addEventListener('load', () => {
+    const calcWindChill = (temperature, speed) => {
+        if (temperature <= 50 && speed > 3) {
+            return Math.round(
+                35.74 + (0.6215 * temperature) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * (temperature *
+                    Math.pow(speed, 0.16)))) + "°F"
+        } else {
+            return "NA"
+        }
+    }
+
+    const displayWindChill = () => {
+        let temperature = parseInt(document.getElementById("temp").textContent);
+        let wind = parseInt(document.getElementById("windSpeed").textContent);
+        let result = calcWindChill(temperature, wind);
+        document.getElementById("windChill").innerHTML = result;
+    }
+    displayWindChill();
+})
