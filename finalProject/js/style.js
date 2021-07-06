9519b82d0398cd69806f2cd83d710f21e7b067dc
KrystianeOtis <oti19001@byui.edu>
2021-07-06 15:40:28 -0600

    side nav bar for final project

--- finalProject/js/style.js
+++ finalProject/js/style.js
@@ -1,9 +1,38 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
+
+// let arrow = document.querySelectorAll(".arrow");
+// console.log(arrow);
+// for (var i = 0; i < arrow.length; i++) {
+//     const element = arrow[i].addEventListener("click", (e) => {
+
+//         let arrowParent = e.target.parentElement.parentElement;
+//         console.log(e);
+//         arrowParent.classList.toggle("showMenu");
+//     });
+
+// }
+// let sidebar = document.querySelector(".sidebar");
+// console.log(arrowParent);
+
+ let arrow = document.querySelectorAll(".arrow");
+ for (var i = 0; i < arrow.length; i++) {
+     arrow[i].addEventListener("click", (e) => {
+         let arrowParent = e.target.parentElement.parentElement;
+         arrowParent.classList.toggle("showMenu");
+     });
+ }
+
+ let sidebar = document.querySelector(".sidebar");
+ let sidebarBtn = document.querySelector(".bx-menu");
+ console.log(sidebarBtn);
+ sidebarBtn.addEventListener("click", () => {
+     sidebar.classList.toggle("close");
+ });
