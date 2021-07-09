809cb57ede0876daa8847fc01439ef3549f5ea90
KrystianeOtis <oti19001@byui.edu>
2021-07-08 20:56:51 -0600

    still nav bar....I want to give up....

--- finalProject/js/style.js
+++ finalProject/js/style.js
@@ -1,38 +1,47 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-// function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//     } else {
-//         x.className = "topnav";
-//     }
-// }
+// window.addEventListener('load', () => {
+//     const hambutton = document.querySelector('.ham');
+//     const mainnav = document.querySelector('#navigation');
 
-// let arrow = document.querySelectorAll(".arrow");
-// console.log(arrow);
-// for (var i = 0; i < arrow.length; i++) {
-//     const element = arrow[i].addEventListener("click", (e) => {
+//     hambutton.addEventListener('click', () => {
+//         mainnav.classList.toggle('responsive')
+//     }, false);
+// });
+ // search-box open close js code
+ let navbar = document.querySelector(".navbar");
+ let searchBox = document.querySelector(".search-box .bx-search");
+ // let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
-//         let arrowParent = e.target.parentElement.parentElement;
-//         console.log(e);
-//         arrowParent.classList.toggle("showMenu");
-//     });
+ searchBox.addEventListener("click", () => {
+     navbar.classList.toggle("showInput");
+     if (navbar.classList.contains("showInput")) {
+         searchBox.classList.replace("bx-search", "bx-x");
+     } else {
+         searchBox.classList.replace("bx-x", "bx-search");
+     }
+ });
 
-// }
-// let sidebar = document.querySelector(".sidebar");
-// console.log(arrowParent);
-
- let arrow = document.querySelectorAll(".arrow");
- for (var i = 0; i < arrow.length; i++) {
-     arrow[i].addEventListener("click", (e) => {
-         let arrowParent = e.target.parentElement.parentElement;
-         arrowParent.classList.toggle("showMenu");
-     });
+ // sidebar open close js code
+ let navLinks = document.querySelector(".nav-links");
+ let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+ let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+ menuOpenBtn.onclick = function () {
+     navLinks.style.left = "0";
+ }
+ menuCloseBtn.onclick = function () {
+     navLinks.style.left = "-100%";
  }
 
- let sidebar = document.querySelector(".sidebar");
- let sidebarBtn = document.querySelector(".bx-menu");
- console.log(sidebarBtn);
- sidebarBtn.addEventListener("click", () => {
-     sidebar.classList.toggle("close");
- });
+
+ // sidebar submenu open close js code
+ let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+ htmlcssArrow.onclick = function () {
+     navLinks.classList.toggle("show1");
+ }
+ let moreArrow = document.querySelector(".more-arrow");
+ moreArrow.onclick = function () {
+     navLinks.classList.toggle("show2");
+ }
+ let jsArrow = document.querySelector(".js-arrow");
+ jsArrow.onclick = function () {
+     navLinks.classList.toggle("show3");
+ }
