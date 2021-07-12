1f8a15ccb8d78aebf337b0315eb68d50ee35b2b3
KrystianeOtis <oti19001@byui.edu>
2021-07-12 17:45:45 -0600

    nav bar with drop down in large view

--- finalProject/js/style.js
+++ finalProject/js/style.js
@@ -5,43 +5,13 @@
 //     hambutton.addEventListener('click', () => {
 //         mainnav.classList.toggle('responsive')
 //     }, false);
+
+//     //To solve the mid resizing issue with responsive
+//     //window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 // });
- // search-box open close js code
- let navbar = document.querySelector(".navbar");
- let searchBox = document.querySelector(".search-box .bx-search");
- // let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
- searchBox.addEventListener("click", () => {
-     navbar.classList.toggle("showInput");
-     if (navbar.classList.contains("showInput")) {
-         searchBox.classList.replace("bx-search", "bx-x");
-     } else {
-         searchBox.classList.replace("bx-x", "bx-search");
-     }
- });
-
- // sidebar open close js code
- let navLinks = document.querySelector(".nav-links");
- let menuOpenBtn = document.querySelector(".navbar .bx-menu");
- let menuCloseBtn = document.querySelector(".nav-links .bx-x");
- menuOpenBtn.onclick = function () {
-     navLinks.style.left = "0";
- }
- menuCloseBtn.onclick = function () {
-     navLinks.style.left = "-100%";
- }
-
-
- // sidebar submenu open close js code
- let htmlcssArrow = document.querySelector(".htmlcss-arrow");
- htmlcssArrow.onclick = function () {
-     navLinks.classList.toggle("show1");
- }
- let moreArrow = document.querySelector(".more-arrow");
- moreArrow.onclick = function () {
-     navLinks.classList.toggle("show2");
- }
- let jsArrow = document.querySelector(".js-arrow");
- jsArrow.onclick = function () {
-     navLinks.classList.toggle("show3");
- }
+function toggleMobileMenu(){
+    document.querySelector('#menu').classList.toggle('active')
+    document.querySelector('.mobile-bar').classList.toggle('active')
+    document.querySelector('.menu-sub').classList.toggle('active')
+}
