2be86602f29bd37e279bf7e91227791690cfdd9d
KrystianeOtis <oti19001@byui.edu>
2021-07-12 18:53:18 -0600

    redid nav bar to something better

--- finalProject/js/style.js
+++ finalProject/js/style.js
@@ -1,17 +1,23 @@
-// window.addEventListener('load', () => {
-//     const hambutton = document.querySelector('.ham');
-//     const mainnav = document.querySelector('#navigation');
-
-//     hambutton.addEventListener('click', () => {
-//         mainnav.classList.toggle('responsive')
-//     }, false);
-
-//     //To solve the mid resizing issue with responsive
-//     //window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-// });
-
-function toggleMobileMenu(){
-    document.querySelector('#menu').classList.toggle('active')
-    document.querySelector('.mobile-bar').classList.toggle('active')
-    document.querySelector('.menu-sub').classList.toggle('active')
-}
+(function ($) { // Begin jQuery
+    $(function () { // DOM ready
+        // If a link has a dropdown, add sub menu toggle.
+        $('nav ul li a:not(:only-child)').click(function (e) {
+            $(this).siblings('.nav-dropdown').toggle();
+            // Close one dropdown when selecting another
+            $('.nav-dropdown').not($(this).siblings()).hide();
+            e.stopPropagation();
+        });
+        // Clicking away from dropdown will remove the dropdown class
+        $('html').click(function () {
+            $('.nav-dropdown').hide();
+        });
+        // Toggle open and close nav styles on click
+        $('#nav-toggle').click(function () {
+            $('nav ul').slideToggle();
+        });
+        // Hamburger to X toggle
+        $('#nav-toggle').on('click', function () {
+            this.classList.toggle('active');
+        });
+    }); // end DOM ready
+})(jQuery); // end jQuery
