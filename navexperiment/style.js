5bd0e394c307f5290925e183d2e951e0c525bdc7
KrystianeOtis <oti19001@byui.edu>
2021-07-14 15:03:53 -0600

    background and service link fix

--- navexperiment/style.js
+++ navexperiment/style.js
@@ -43,3 +43,10 @@
     floatLabel(".floatLabel");
 })(jQuery);
 
+
+//   $('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+
+//   $("#menu-icon").on("click", function () {
+//       $("nav").slideToggle();
+//       $(this).toggleClass("active");
+//   });
